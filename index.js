b52db94556d63717eb45b3644d7b3df342f0b64d
Lougenia <lougenia.bailey@gmail.com>
2016-08-03 23:57:24 -0400

    Committed broken code in an attempt to select from a rethinkdb table.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http')
 const port = 7000
+var r = require('./server/db')
 
 const requestHandler = function (request, response) {
   console.log(request.url)
-  response.end('Day 2 of 100 commits!')
+  // response.end('Day 2 of 100 commits!')
+  response.end(r.table('todo'))
 }
 
 const server = http.createServer(requestHandler)
--- server/db.js
+++ server/db.js
@@ -1,28 +1,51 @@
 r = require('rethinkdb');
 
-var connection = null;
-r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+const connection = null;
+r.connect( {host: 'localhost', port: 7000}, function(err, conn) {
     if (err) throw err;
     connection = conn;
+
+    r.db('test').tableCreate('todo').run(connection, function(err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result, null, 2));
+    })
+
+    r.table('todo').insert([
+        {
+          name: 'clean',
+          dueDate: '8/5/16',
+          status: 'open'
+        },
+        {
+          name: 'first commit',
+          dueDate: '8/1/16',
+          status: 'closed'
+        }
+    ]).run(connection, function(err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result, null, 2));
+    })
 })
 
-r.db('test').tableCreate('todo').run(connection, function(err, result) {
-    if (err) throw err;
-    console.log(JSON.stringify(result, null, 2));
-})
+// r.db('test').tableCreate('todo').run(connection, function(err, result) {
+//     if (err) throw err;
+//     console.log(JSON.stringify(result, null, 2));
+// })
 
-r.table('todo').insert([
-    {
-      name: 'clean',
-      dueDate: '8/5/16',
-      status: 'open'
-    },
-    {
-      name: 'first commit',
-      dueDate: '8/1/16',
-      status: 'closed'
-    }
-]).run(connection, function(err, result) {
-    if (err) throw err;
-    console.log(JSON.stringify(result, null, 2));
-})
+// r.table('todo').insert([
+//     {
+//       name: 'clean',
+//       dueDate: '8/5/16',
+//       status: 'open'
+//     },
+//     {
+//       name: 'first commit',
+//       dueDate: '8/1/16',
+//       status: 'closed'
+//     }
+// ]).run(connection, function(err, result) {
+//     if (err) throw err;
+//     console.log(JSON.stringify(result, null, 2));
+// })
+
+module.exports = r;
