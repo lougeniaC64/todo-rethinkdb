128990eb7a6ee92679098310bc87183e15a7f8e6
Lougenia <lougenia.bailey@gmail.com>
2016-08-09 23:53:45 -0400

    Attempted to add rethinkdb table to express server

--- app/index.js
+++ app/index.js
@@ -1,19 +1,72 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const expressPort = 3000
 
-app.get('/', function (request, response) {
-  response.send('Hello from Express!')
+var r = require('rethinkdb')
+const config = { rethinkdb: { host: 'localhost', port: 8080} }
+
+
+
+app.use(createConnection);
+
+app.get('/todo/get', get);
+
+app.use(closeConnection);
+
+r.connect(config.rethinkdb , function(err, conn) {
+    if (err) {
+        console.log("Could not open a connection to initialize the database");
+        throw err;
+    }
+
+    // r.table('todo').indexWait('createdAt').run(conn).then(function(err, result) {
+    //     console.log("Table and index are available, starting express...");
+    //     startExpress();
+    // }).error(function(err) {
+    //   // r.db('test').tableCreate('todo').run(conn, function(err, result) {
+    //   //   if (err) {
+    //   //     throw err;
+    //   //   }
+    //   //
+    //   //   startExpress()
+    //   //   console.log(JSON.stringify(result, null, 2));
+    //   // })
+    // });
+
+
 })
 
-app.get('/todos', function (request, response) {
-  response.send(['this', 'that', 'the other'])
-})
+function createConnection(req, res, next) {
+    r.connect(config.rethinkdb).then(function(conn) {
+        req._rdbConn = conn;
+        next();
+    });//.error(handleError(res));
+}
 
-app.listen(port, function (err) {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+function closeConnection(req, res, next) {
+    req._rdbConn.close();
+}
 
-  console.log('server is listening on ' + port)
-})
+function get(req, res) {
+  res.send(['me', 'you', 'them'])
+}
+
+function startExpress() {
+    app.listen(expressPort);
+    console.log('Listening on port ' + expressPort);
+}
+// app.get('/', function (request, response) {
+//   response.send('Hello from Express!')
+// })
+//
+// app.get('/todos', function (request, response) {
+//   response.send(['this', 'that', 'the other'])
+// })
+//
+// app.listen(expressPort, function (err) {
+//   if (err) {
+//     return console.log('something bad happened', err)
+//   }
+//
+//   console.log('server is listening on ' + port)
+// })
