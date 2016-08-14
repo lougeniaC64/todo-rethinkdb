9ae3910a9d99639db037e7f70d4f604c3add674a
Lougenia <lougenia.bailey@gmail.com>
2016-08-14 19:20:56 -0400

    Added db name to rethinkdb config

--- app/config.js
+++ app/config.js
@@ -1,6 +1,8 @@
 var config = {
-  rethinkdb: { host: 'localhost', port: 28015 },
-  express: { port: 3000 } 
+  rethinkdb: { host: 'localhost',
+                port: 28015,
+                db: 'test' },
+  express: { port: 3000 }
 };
 
 module.exports = config;
